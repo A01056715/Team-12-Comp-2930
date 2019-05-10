02a9b1d4cad3bff08e18c3d1f02978d6ebe32e43
Kenichi Okiebisu <a01056715@gmail.com>
2019-05-09 23:24:08 -0700

    successfully deployed

--- conquez/china/models.py
+++ conquez/china/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    #Image
-    image = models.ImageField(upload_to='images/')
     #Question
     question = models.CharField(max_length=200, default="")
     #Explanation
