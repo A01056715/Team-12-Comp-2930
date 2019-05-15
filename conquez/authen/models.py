e9dbf79303ea7635b3c6c29af5102281ef61543a
Kenichi Okiebisu <a01056715@gmail.com>
2019-05-15 00:49:19 -0700

    modified models

--- conquez/authen/models.py
+++ conquez/authen/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from country.models import Country
 
 # Create your models here.
+class Conquered(models.Model):
+    country_id=models.ForeignKey(Country, on_delete=models.CASCADE)
