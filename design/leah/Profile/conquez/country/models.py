ea31a06a1dadc108d1f8328aa5b80c7d253d4109
Younga Park <duddk226@gmail.com>
2019-05-19 23:39:26 -0700

    profile page, may continue if time

---
+++ design/leah/Profile/conquez/country/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    question = models.CharField(max_length=100)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    choice1 = models.CharField(max_length=50)
+    choice2 = models.CharField(max_length=50)
+    choice3 = models.CharField(max_length=50)
+    choice4 = models.CharField(max_length=50)
+    answer = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.question
+
